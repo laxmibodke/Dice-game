9eb2a09d8c802daa216967fa0f036f0199fc76c3
laxmibodke <laxmi.bodke22@spit.ac.in>
2024-09-08 18:19:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage = "dice" +randomNumber1 +".png";
+
+var randomDiceImageSource = "images/" +randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomDiceImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "dice" +randomNumber2 +".png";
+
+var randomDiceImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomDiceImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!"
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 wins!" 
+}else{
+    document.querySelector("h1").innerHTML = "Ohh! It's draw" 
+}
